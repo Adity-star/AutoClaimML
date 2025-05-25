6ee525fe8a1c09e0f41a61bead83d3d974303132
Aditya Akuskar <Adiiii@Aditya.Akuskar>
2025-05-26 03:03:25 +0530

    working on data ingestion

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -1,1 +1,32 @@
 # config_entity.py
+import os
+from src.constants import *
+from dataclasses import dataclass 
+from datetime import datetime 
+
+# Generate a timestamp for versioned artifacts
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+@dataclass
+class TrainingPipelineConfig:
+    """
+    Configuration for the overall ML pipeline.
+    """
+    pipeline_name: str = PIPELINE_NAME
+    artifact_dir: str = os.path.join(ARTIFACT_DIR, TIMESTAMP)
+    timestamp: str = TIMESTAMP
+
+
+
+@dataclass
+class DataIngestionConfig:
+    """
+    Configuration for the data ingestion stage of the pipeline.
+    """
+    data_ingestion_dir: str
+    feature_store_file_path: str
+    training_file_path: str
+    testing_file_path: str
+    train_test_split_ratio: float
+    collection_name: str
+ 
