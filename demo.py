119540fddd1183f52c0bafc265b94862e8d2c875
Aditya Akuskar <adityaakuskar123@gmail.com>
2025-05-26 00:21:45 +0530

    exception handling sucessfull

--- demo.py
+++ demo.py
@@ -1,8 +1,12 @@
 # demo.py
-from src.logger import logging
 
-logging.debug("This is a debug message.")
-logging.info("This is an info message.")
-logging.warning("This is a warning message.")
-logging.error("This is an error message.")
-logging.critical("This is a critical message.")
+
+from src.logger import logging
+from src.exception import CustomException
+import sys
+
+try:
+     a = 1+'Z'
+except Exception as e:
+     logging.info(e)
+     raise CustomException(e, sys) from e
