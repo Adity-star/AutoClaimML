2480a0e8d1cb99ce478a38ec8bae94cc723121b8
Aditya Akuskar <Adiiii@Aditya.Akuskar>
2025-05-26 16:33:12 +0530

    ready till validation configuration

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -7,24 +7,25 @@ from datetime import datetime
 
 @dataclass
 class TrainingPipelineConfig:
-    """
-    Configuration for the overall ML pipeline.
-    """
+
     pipeline_name: str = PIPELINE_NAME
     artifact_dir: str = os.path.join(ARTIFACT_DIR)
     
 
-
-
 @dataclass
 class DataIngestionConfig:
-    """
-    Configuration for the data ingestion stage of the pipeline.
-    """
+
     data_ingestion_dir: str
     feature_store_file_path: str
     training_file_path: str
     testing_file_path: str
     train_test_split_ratio: float
     collection_name: str
+
+@dataclass
+class DataValidationConfig:
+
+    data_validation_dir: str
+    report_file_path: str
+    schema_file_path: str
  
